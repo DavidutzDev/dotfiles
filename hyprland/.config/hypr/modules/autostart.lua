17ddca52fca261511583320773d6563615fea5a3
David Gheghea <71325082+DavidutzDev@users.noreply.github.com>
2026-05-28 01:37:29 +0200

    work in progress

---
+++ hyprland/.config/hypr/modules/autostart.lua
@@ -0,0 +1,20 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+
+hl.on("hyprland.start", function()
+  hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+  hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+  hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland &")
+end)
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+-- hl.on("hyprland.start", function () 
+--   hl.exec_cmd(terminal)
+--   hl.exec_cmd("nm-applet")
+--   hl.exec_cmd("waybar & hyprpaper & firefox")
+-- end)
